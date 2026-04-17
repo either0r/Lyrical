c54e645c860e4118ff9e2b75fe05e253c97336c2
either0r <either0r@outlook.com>
2026-04-17 17:48:29 -0400

    fixed multiple document loading, fixed app bar icon/theme modes.

---
+++ Lyrical/Models/Glyphs.cs
@@ -0,0 +1,14 @@
+﻿namespace Lyrical.Models
+{
+    public static class Glyphs
+    {
+        public const string Verse = "\uf000";
+        public const string SoloTab = "\uf001";
+        public const string Metadata = "\uf002";
+        public const string Chorus = "\uf003";
+        public const string Capo = "\uf004";
+        public const string MoreTab = "\uf005";
+        public const string guitar = "\ue954";
+        public const string Music = "\ue94D";
+    }
+}
